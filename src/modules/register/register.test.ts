8ae9e00cd870826cac090c71f9a6c90286942089
Evan N <dacsang97@gmail.com>
2018-12-03 18:58:36 +0700

    chore - move register test to modules

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,14 +1,14 @@
 import { request } from "graphql-request";
-import { User } from "../entity/User";
-import { startServer } from "../startServer";
 import { AddressInfo } from "net";
+import { startServer } from "../../startServer";
+import { User } from "../../entity/User";
 
-let getHost = () => "";
+let getHost = "";
 
 beforeAll(async () => {
   const app = await startServer();
   const { port } = app.address() as AddressInfo;
-  getHost = () => `http://127.0.0.1:${port}`;
+  getHost = `http://127.0.0.1:${port}`;
 });
 
 const email = "dacsang971@gmail.com";
@@ -20,7 +20,7 @@ mutation {
 `;
 
 test("Register user", async () => {
-  const response = await request(getHost(), mutation);
+  const response = await request(getHost, mutation);
   expect(response).toEqual({ register: true });
   const users = await User.find({ where: { email } });
   expect(users.length).toEqual(1);
