b4d19698a379328b25cb0c2670acef373660fc17
Evan N <dacsang97@gmail.com>
2018-12-03 19:32:09 +0700

    feat - check user email already taken

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -15,16 +15,27 @@ const email = "dacsang971@gmail.com";
 const password = "123456";
 const mutation = `
 mutation {
-  register(email: "${email}", password: "${password}")
+  register(email: "${email}", password: "${password}") {
+    path
+    message
+  }
 }
 `;
 
+interface IRegisterResponse {
+  register: any[];
+}
+
 test("Register user", async () => {
   const response = await request(getHost, mutation);
-  expect(response).toEqual({ register: true });
+  expect(response).toEqual({ register: null });
   const users = await User.find({ where: { email } });
   expect(users.length).toEqual(1);
   const user = users[0];
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
+
+  const response2 = await request<IRegisterResponse>(getHost, mutation);
+  expect(response2.register.length).toEqual(1);
+  expect(response2.register[0].path).toEqual("email");
 });
